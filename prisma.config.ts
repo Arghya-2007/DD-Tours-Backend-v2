baebd0dc50a7d992ad38f10747028e7d2ce046b3
Arghya Pal <arghya.github@gmail.com>
2026-02-14 09:52:15 +0530

    Remove unnecessary prisma config file

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@prisma/config';
-import 'dotenv/config';
-
-export default defineConfig({
-    schema: 'prisma/schema.prisma',
-    datasource: { // ✅ Singular
-        url: process.env.DATABASE_URL,
-    },
-});
